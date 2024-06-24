f8ab601b998ebb697310bcac83a27f3c9be3349f
iraychev <ivoraychev1337@gmail.com>
2024-06-24 15:07:03 +0300

    add search to listings

--- src/app/components/listings/listings.component.ts
+++ src/app/components/listings/listings.component.ts
@@ -5,16 +5,20 @@ import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { ButtonComponent } from '../../shared/button/button.component';
+import { FormsModule } from '@angular/forms';
+import { Listing } from '../../models/listing.model';
 
 @Component({
   selector: 'app-listings',
   templateUrl: './listings.component.html',
   standalone: true,
-  imports: [CommonModule, RouterLink, ButtonComponent],
+  imports: [CommonModule, RouterLink, ButtonComponent, FormsModule],
   styleUrls: ['./listings.component.css'],
 })
 export class ListingsComponent implements OnInit {
-  listings: any[] = [];
+  listings: Listing[] = [];
+  filteredListings: Listing[] = [];
+  searchTerm: string = ''; 
   errorMessage: string = '';
 
   constructor(
@@ -32,9 +36,23 @@ export class ListingsComponent implements OnInit {
     this.apiService.fetchListings().subscribe({
       next: (data) => {
         this.listings = data;
+        this.filteredListings = [...this.listings];
       },
       error: (err) => console.error('Error fetching listings', err),
     });
+  }
+
+  performSearch() {
+    if (this.searchTerm.trim() === '') {
+      this.filteredListings = [...this.listings];
+    } else {
+      const lowerCaseSearchTerm = this.searchTerm.trim().toLowerCase();
+
+      this.filteredListings = this.listings.filter(listing =>
+        listing.title.toLowerCase().includes(lowerCaseSearchTerm) ||
+        listing.propertyAddress.toLowerCase().includes(lowerCaseSearchTerm)
+      );
+    }
   }
 
   viewDetails(listingId: string): void {
